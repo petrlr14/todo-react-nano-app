b2a2e9e57e8063fb5dac6b76a32fa3b9cc197d45
petrlr14 <pedrodeveloper14@gmail.com>
2019-07-20 13:50:03 -0600

    micro app base with tailwind.css

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <div className="">
+    <h1 className="">Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </div>
+);
